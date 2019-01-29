ca9bfb4c35d7d8f63ed482a20befd44a1750a488
brandiw <brandimichelle.0911@gmail.com>
2019-01-29 12:08:01 -0800

    Prepping code for deployment

---
+++ models/index.js
@@ -0,0 +1,10 @@
+// Require mongoose
+const mongoose = require('mongoose');
+
+// Connect to mongodb
+// NOTE: Heroku mongo add-on creates this environment variable: MONGODB_URI
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/hunters', { useNewUrlParser: true });
+
+// Include and export all models in this folder
+// NOTE: Include as many as needed in this same format
+module.exports.Bounty = require('./bounty');
